cb01195f1cbcc0edd8138f7099ddb09657a1e512
elrincondehiro <administracion@elrincondehiro.com>
2024-11-14 14:01:31 +0100

    vercel correct?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({

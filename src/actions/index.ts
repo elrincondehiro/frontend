9c5b3f1877ac414075a2b0ccf7e883d80518dead
elrincondehiro <administracion@elrincondehiro.com>
2024-11-15 20:44:20 +0100

    add some logic add to cart

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,4 @@
-import { defineAction } from 'astro:actions';
+import { actions, defineAction } from 'astro:actions';
 import { z } from 'astro:schema';
 
 export const server = {
@@ -20,6 +20,31 @@ export const server = {
         console.error('Error checking stock:', error);
         throw error; // O manejar el error de otra manera según tus necesidades
       }
+    }
+  }),
+  addToCart: defineAction({
+    input: z.string(),
+    handler: async (input, context) => {
+      try {
+        // Verificar el stock del producto en el backend
+        const {data, error} = await actions.checkStock(input);
+        console.log('stockAvailable', data);
+        if (!error && data) {
+          // Agregar el producto al carrito
+          
+          console.log('item añadido');
+          return true;
+        } else {
+          // Mostrar un mensaje de error indicando que no hay stock disponible
+          console.log('Lo sentimos, no hay stock disponible de este producto.');
+          return false;
+        }
+      } catch (error) {
+        console.error('Error al agregar el producto al carrito:', error);
+        throw error;
+        // Manejar el error apropiadamente, como mostrar un mensaje de error al usuario
+      }
+
     }
   }),
 }

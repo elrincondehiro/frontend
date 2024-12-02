7a520c2cbc15f5b8bd434b7128b712fafd1971e5
elrincondehiro <administracion@elrincondehiro.com>
2024-12-02 12:52:25 +0100

    ENV CONFIG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
 
@@ -9,5 +9,10 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   output: 'server',
   integrations: [tailwind(), svelte({ extensions: ['.svelte'] })],
+  env: {
+    schema: {
+      BACK_END_URL: envField.string({ default: "http://localhost:1337", context: 'client', access: 'public'})
+    }
+  },
   adapter: vercel()
 });
